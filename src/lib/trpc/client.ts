2c36950a76b0e9ec225d44d26a6239cdaf326f62
Bruno Lucas <leemon.studio@gmail.com>
2025-07-30 17:15:17 +0000

    no funciono, recurre a un metodo coherente con nuestra app, sigue la mis

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,4 +1,19 @@
-import { createTRPCReact } from "@trpc/react-query";
+"use client";
+
+import { createTRPCReact, httpBatchLink } from "@trpc/react-query";
 import type { AppRouter } from "./server";
 
 export const trpc = createTRPCReact<AppRouter>({});
+
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: `/api/trpc`,
+      fetch: (url, options) =>
+        fetch(url, {
+          ...options,
+          credentials: 'include',
+        }),
+    }),
+  ],
+});
