9ea49a4e2bdd733c3548cf71a62c00f06f7d0878
Bruno Lucas <leemon.studio@gmail.com>
2025-07-30 22:35:39 +0000

    cuando quise crear un datapack nuevo: boom, no funciona!

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -11,6 +11,7 @@ const serviceAccount = {
 const app = !getApps().length
   ? initializeApp({
       credential: cert(serviceAccount),
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     })
   : getApp();
 
