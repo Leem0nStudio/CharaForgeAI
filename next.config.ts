506b60929cb57f2227b7dd4dc374a72c9f9145c9
Bruno Lucas <leemon.studio@gmail.com>
2025-07-31 02:40:54 +0000

    ingresar con google: llega hasta esa pantalla, pero al darle aceptar , l

--- next.config.ts
+++ next.config.ts
@@ -22,10 +22,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Configure allowedDevOrigins for Firebase Studio preview
-  devServer: {
-    allowedDevOrigins: ['https://6000-firebase-studio-1753843934284.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev', 'https://6000-firebase-studio-1753843934284.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev'],
-  },
 };
 
 export default nextConfig;
